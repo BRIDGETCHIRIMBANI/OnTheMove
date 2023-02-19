ae153499b248f48305c5337c54da553266987ab2
BRIDGETCHIRIMBANI <bridgetchirimbani22@gmail.com>
2023-02-19 14:05:38 +0400

    add home component

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import React from "react";
 import Footer from "./components/Footer/Footer";
 import Navbar from "./components/Nav/Navbar";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/Home/Home";
+import Contact from "./components/Contacts/Contact";
 
 export default function App() {
   return (
     <div>
       <Navbar/>
       <Router>
-      
-
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/contact" element={<Contact/>}/>
+        </Routes>
     </Router>
     <Footer/>
     </div>
