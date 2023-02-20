d5576cb91ae6fc5582c5d0023fc4334f48907da4
BRIDGETCHIRIMBANI <bridgetchirimbani22@gmail.com>
2023-02-20 12:05:01 +0400

    Update Contact Form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import React from "react";
-import {Form, Input, TextArea, Button} from 'semantic-ui-react';
 import Footer from "./components/Footer/Footer";
 import Navbar from "./components/Nav/Navbar";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home/Home";
 import Contact from "./components/Contacts/Contact";
+import "./index.css";
 
 export default function App() {
+  
   return (
     <div>
       <Navbar/>
-    {/* <Router> 
+    <Router> 
         <Routes>
-          <Route path="/" element={<Home/>}/>
-          <Route path="/home" element={<Globe/>}/>
+          <Route exact path="/" element={<Home/>}/>
+         {/* <Route path="/home" element={<Globe/>}/>
           <Route path="/contact" element={<Continent/>}/>
           <Route path="/contact" element={<CurrencyConverter/>}/>
-          <Route path="/contact" element={<Articles/>}/>
-          <Route path="/contact" element={<Contact/>}/>
+          <Route path="/contact" element={<Articles/>}/> */}
+          <Route exact path="/contact" element={<Contact/>}/>
         </Routes>
-    </Router> */}
+    </Router> 
 
      <Footer/> 
     </div>
