14a49c437acfe47769048a6e148c6be4786d212f
BRIDGETCHIRIMBANI <bridgetchirimbani22@gmail.com>
2023-03-23 04:03:21 +0400

    Add Bootstrap on LearnMore page

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,27 @@
 export default function Footer () {
     return (
-<footer class="text-center text-lg-start bg-light text-muted">
- {/* <section class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
-    <div class="me-5 d-none d-lg-block">
-      <span>Get connected with us on social networks:</span>
+<footer className="text-center text-lg-start bg-light text-muted">
+  
+  <section className="">
+    <div className="container text-center text-md-start mt-5">
+      <div className="row mt-3">
+        <div className="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+        <h6 className="text-uppercase fw-bold mb-4"><a href="#!" class="text-reset">About Us</a></h6>
+        <div className="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+        <h6 className="text-uppercase fw-bold mb-4"><a href="#!" class="text-reset">Contact Us</a></h6>
+        <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+        <h6 className="text-uppercase fw-bold mb-4"><a href="#!" class="text-reset">Help</a></h6>
+        </div>
+        </div>
+        </div>
+      </div>
     </div>
-    
+  </section>
+  
+  <section className="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
+    <div className="me-5 d-none d-lg-block">
+      <span>Follow OnTheMove on:</span>
+    </div>
     <div>
       <a href="" className="me-4 text-reset">
         <i class="fab fa-facebook-f"></i>
@@ -14,67 +30,11 @@ export default function Footer () {
         <i class="fab fa-twitter"></i>
       </a>
       <a href="" className="me-4 text-reset">
-        <i class="fab fa-google"></i>
-      </a>
-      <a href="" className="me-4 text-reset">
-        <i class="fab fa-instagram"></i>
-      </a>
-      <a href="" class="me-4 text-reset">
         <i class="fab fa-linkedin"></i>
       </a>
-      <a href="" class="me-4 text-reset">
-        <i class="fab fa-github"></i>
-      </a>
     </div>
-  </section> */}
-  
-  <section class="">
-    <div class="container text-center text-md-start mt-5">
-      <div class="row mt-3">
-        <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
-          <h6 class="text-uppercase fw-bold mb-4">
-            <i class="fas fa-gem me-3"></i> Articles </h6>
-          <p>What is the cost of Living?</p>
-          <p>Life then and now, how tech has changed our lives?</p>    
-          <p>Sanctions in Africa and its impact on the economy?</p>
-          <p>Zimbabwe 2023 elections</p>
-        </div>
+  </section> 
 
-       
-        
-        <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
-
-          <h6 class="text-uppercase fw-bold mb-4">
-            Useful links
-          </h6>
-          <p>
-            <a href="#!" class="text-reset">Home</a>
-          </p>
-          <p>
-            <a href="#!" class="text-reset">Globe</a>
-          </p>
-          <p>
-            <a href="#!" class="text-reset">Currency Converter</a>
-          </p>
-          <p>
-            <a href="#!" class="text-reset">Contact Us</a>
-          </p>
-        </div>
-        
-        <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
-          <h6 class="text-uppercase fw-bold mb-4">Contact Us</h6>
-          <p><i class="fas fa-home me-3"></i> OnTheMove </p>
-          <p>
-            <i class="fas fa-envelope me-3"></i>
-            b.chirimban@alustudent.com
-          </p>
-          <p><i class="fas fa-phone me-3"></i> +230 5452 1304</p>
-          <p><i class="fas fa-print me-3"></i> + 263 782 724 394</p>
-        </div>
-      </div>
-    </div>
-  </section>
-  
   <div class="text-center p-4">
     © 2021 Copyright:
     <a class="text-reset fw-bold" href="https://mdbootstrap.com/">MDBootstrap.com</a>
