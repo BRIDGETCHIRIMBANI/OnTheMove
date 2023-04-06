02828dbff4d4b099293f220065bf025b06eb5898
BRIDGETCHIRIMBANI <b.chirimban@alustudent.com>
2023-04-06 13:16:36 +0400

    Country page update with cost of living comparison

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -3,22 +3,28 @@ import { useEffect, useState } from "react";
 export default function Country () {
 
     //define a state variable called countries using the UseState hook. It's intial value is an empty array.//
+    const [city1, setCity1] = useState('');
+    const [city2, setCity2] = useState('');
     const [countries, setCountries]=useState([]);
+    const [cost1, setCost1] = useState({});
+    const [cost2, setCost2] = useState({});
 
     //HandleSubmit is a function that takes an event as an argument(It is triggered by an event such as when a user clicks on a button)
     const handleSubmit = async (event) => {
       event.preventDefault();
-      const City1 = document.getElementByID ('city1').value;
-      const City2 = document.getElementByID ('city2').value;
-      const City1Array = City1.split('-');
-      const City2Array = City2.split('-');
+      const City1Array = city1.split('-');
+      const City2Array = city2.split('-');
 
-      const cityPrices = await getPrices();
-      console.log(cityPrices);
+      console.log(city1);
+      await getPrices(City1Array[0], City1Array[1]);
+      await getPrices2(City2Array[0], City2Array[1]);
+      // setPrices(await getPrices());
+      console.log(cost1);
+      console.log(cost2);
 
     }
 
-    const getPrices = async () => {
+    const getPrices = async (city, country) => {
         const options = {
             method: 'GET',
             headers: {
@@ -27,11 +33,35 @@ export default function Country () {
             }
         };
         
-        fetch('https://cost-of-living-and-prices.p.rapidapi.com/prices?city_name=Bratislava&country_name=Slovakia', options)
-	    .then(response => response.json())
-	    .then(response => {return response})
+        await fetch(`https://cost-of-living-and-prices.p.rapidapi.com/prices?city_name=${city}&country_name=${country}`, options)
+	    .then(response => {
+        return response.json()
+      })
+	    .then(data => {
+        setCost1(data);
+      })
 	    .catch(err => console.error(err));
     }
+
+    const getPrices2 = async (city, country) => {
+      const options = {
+          method: 'GET',
+          headers: {
+              'X-RapidAPI-Key': 'a95b39b821msh95dd7fa69aca5b2p116b83jsnaef79a653966',
+              'X-RapidAPI-Host': 'cost-of-living-and-prices.p.rapidapi.com'
+          }
+      };
+      
+      await fetch(`https://cost-of-living-and-prices.p.rapidapi.com/prices?city_name=${city}&country_name=${country}`, options)
+    .then(response => {
+      return response.json()
+    })
+    .then(data => {
+      setCost2(data);
+    })
+    .catch(err => console.error(err));
+  }
+    console.log(cost1);
 
     const getCities = async () => {
         const options = { 
@@ -41,22 +71,37 @@ export default function Country () {
             'X-RapidAPI-Host': 'country-facts.p.rapidapi.com'
           }
         };
-        const response = await fetch('https://country-facts.p.rapidapi.com/all', options);
-        const data = await response.json();
-        return data;
-      };
-      
-      useEffect(() => {
-        const fetchCountries = async () => {
+
+            const response = await fetch('https://country-facts.p.rapidapi.com/all', options);
+            const data = await response.json();
+            return data;
+        };
+         
+      function handleCity1Change(event) {
+        setCity1(event.target.value);
+      }
+
+      function handleCity2Change(event) {
+          setCity2(event.target.value);
+      }
+       
+      useEffect(() => { 
+       const fetchCountries = async () => {
           const countriesData = await getCities();
           setCountries(countriesData);
         };
         fetchCountries();
-      }, []);
-      
+      }, []); 
+
       
     // This is the user-interface that the user will interact with. 
     return (
+      <>
+        <div id="container">
+         <div id="facts"></div>
+         <div id="map"></div>
+        </div>
+      
         <div className="container my-4 mx-auto width-75 p-4 rounded" style={{backgroundColor: "white"}}>
             
             <h2>Cost of Living Comparison</h2> 
@@ -65,21 +110,39 @@ export default function Country () {
         <form onSubmit={handleSubmit} > 
         <div className="row mb-3">
             <div class="col-md 6"> 
-            <input list="cities" type="text" className="form-control border-dark" placeholder="Search City 1" id="city1"></input>
+            <input list="cities" type="text" className="form-control border-dark" placeholder="Search City 1" id="city1" onChange={handleCity1Change}></input>
+
             </div>
+
             <div class="col-md 6"> 
-            <input list="cities" type="text" className="form-control border-dark" placeholder="Search City 2" id="city2"></input>
+            <input list="cities" type="text" className="form-control border-dark" placeholder="Search City 2" id="city2" onChange={handleCity2Change}></input>
+
             </div>
             </div>
-          <button type="button" class="btn btn-outline-search">Search</button>  
+          <button type="submit" class="btn btn-outline-search">Search</button>  
         </form>
+        <table>
+          <tr>
+            <th>Item name</th>
+            <th>Price 1</th>
+            <th>Price 2</th>
+          </tr>
+        {cost1.prices && cost1.prices.map((item, index) => {
+          return <tr key={index}>
+            <td>{item.item_name}</td>
+            <td>{item?.usd?.avg}</td>
+            <td>{cost2?.prices?.filter(c2 => c2.item_name === item.item_name)[0]?.usd?.avg}</td>
+          </tr>
+        })}
+        </table>
+
         <datalist id="cities">
-            {countries && countries.map(country => {
-                return (<option value={country.capital[0]+'-'+country.name.common}/>);
+            {countries && countries.map((country, index) => {
+                return (<option key={index} value={country.capital[0]+'-'+country.name.common}/>);
             })}
         </datalist>
        </div>
+       </>
     );
 
 }
-
